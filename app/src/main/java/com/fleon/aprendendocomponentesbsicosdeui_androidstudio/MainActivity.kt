6ea78585ee3a838ab5d5373ce16fc1b77e3ebec8
FredymarLeon <fredymar@gmail.com>
2024-10-10 14:11:37 -0300

    Foi criado o arquivo .kt para lógica de funcionalidade.  Está apresentando um erro no output.

--- app/src/main/java/com/fleon/aprendendocomponentesbsicosdeui_androidstudio/MainActivity.kt
+++ app/src/main/java/com/fleon/aprendendocomponentesbsicosdeui_androidstudio/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.fleon.aprendendocomponentesbsicosdeui_androidstudio
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
+    val editTextName: EditText by lazy { findViewById(R.id.name) }
+    val editTextAge: EditText by lazy {findViewById(R.id.age)}
+    val outputText: TextView by lazy {findViewById(R.id.output)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setupView()
     }
+
+    private fun setupView() {
+        editTextName.addTextChangedListener { text ->
+            atualizarText(text.toString(), editTextAge.toString())
+        }
+
+        editTextAge.addTextChangedListener { text ->
+            atualizarText(editTextName.text.toString(), text.toString())
+        }
+    }
+
+    private fun atualizarText(nome: String?, idade: String?) {
+            outputText.text = "Olá, eu sou $nome e tenho $idade anos."
+        }
 }
